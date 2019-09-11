8298b057ccb5ee9c2e6db5ee8c9b193472f5f9a1
Nuwan <nuwanmadhusankarulz@gmail.com>
2019-09-11 08:43:02 +0530

    config

--- AngularApp/src/app/app.constants.ts
+++ AngularApp/src/app/app.constants.ts
@@ -1,3 +1,3 @@
-export const API_URL="http://localhost:8080"
+// export const API_URL="http://localhost:8080"
 
-//export const API_URL="http://ec2-52-66-119-69.ap-south-1.compute.amazonaws.com:8080"
+export const API_URL="http://ec2-52-66-119-69.ap-south-1.compute.amazonaws.com:8080"
