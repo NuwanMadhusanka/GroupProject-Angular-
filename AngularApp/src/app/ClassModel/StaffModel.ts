5cfd8d90ad2296a8bec8bb17e05b02dffeee0bab
AnupamaTK <swathathsarani@gmail.com>
2019-12-24 13:25:40 +0530

    revrting

--- AngularApp/src/app/ClassModel/StaffModel.ts
+++ AngularApp/src/app/ClassModel/StaffModel.ts
@@ -3,6 +3,13 @@ import { UserModel } from './UserModel';
 export class StaffModel{
     constructor(
     public staffId:Number,
+<<<<<<< Updated upstream
+=======
+    public name:String,
+      public nic:String,
+      public tel:String,
+      public address:string,
+>>>>>>> Stashed changes
     public userId:UserModel
   ){}
   }
