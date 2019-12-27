23908b49db60845f1e912e2b3b65a9bd12e46f79
Nimesha1996 <nimesha.chathurangi19@gmail.com>
2019-12-27 14:03:02 +0530

    create vehicle-category-add component

---
+++ Client/src/app/vehicle/vehicle-category-add/vehicle-category-add.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VehicleCategoryAddComponent } from './vehicle-category-add.component';
+
+describe('VehicleCategoryAddComponent', () => {
+  let component: VehicleCategoryAddComponent;
+  let fixture: ComponentFixture<VehicleCategoryAddComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ VehicleCategoryAddComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(VehicleCategoryAddComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Client/src/app/vehicle/vehicle-category-add/vehicle-category-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-category-add',
+  templateUrl: './vehicle-category-add.component.html',
+  styleUrls: ['./vehicle-category-add.component.scss']
+})
+export class VehicleCategoryAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
