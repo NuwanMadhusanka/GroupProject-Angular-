f775c21299d4d1710b9e44b26cd6782320c79869
Nimesha1996 <nimesha.chathurangi19@gmail.com>
2020-02-18 22:19:25 +0530

    instructor assign

--- Client/src/app/vehicle/instructor-assign/instructor-assign.component.ts
+++ Client/src/app/vehicle/instructor-assign/instructor-assign.component.ts
@@ -5,7 +5,12 @@ import { VehicleModel } from '../../ClassModel/VehicleModel';
 import { VehicleServiceService } from '../../service/vehicle/vehicle-service.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { HttpError } from '../../Shared/httpError/HttpError';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { InstructorServiceService } from '../../service/instructor/instructor-service.service';
+import { InstructorModel } from '../../ClassModel/InstructorModel';
+import { StaffModel } from '../../ClassModel/StaffModel';
+import { UserModel } from '../../ClassModel/UserModel';
+import { VehicleCategoryModel } from '../../ClassModel/MapObject/VehicleCategory';
 
 @Component({
   selector: 'app-instructor-assign',
@@ -14,11 +19,65 @@ import { Router } from '@angular/router';
 })
 export class InstructorAssignComponent implements OnInit {
 
+
+  vehicleId;
+  errorMessage;
+  httpError = new HttpError();
+
+  user : UserModel = new UserModel (1, '','','','','','','',new Date(),1,1,1);
+  staff: StaffModel = new StaffModel(1, this.user);
+  instructorData:InstructorModel = new InstructorModel(1,'q', this.staff);
+  vehicleCategory: VehicleCategoryModel = new VehicleCategoryModel(1, 'q', 1);
+  vehicleData: VehicleModel = new VehicleModel(1, 'q ', 'q ', 1,1,' q',this.instructorData,1,' q', this.vehicleCategory);
+
+
   constructor(
+    private route: ActivatedRoute,
+    private instructorService: InstructorServiceService,
+    private vehicleService: VehicleServiceService,
     private router:Router,
   ) { }
 
   ngOnInit() {
+    console.log("In InstructorDetails ts");
+    console.log(this.vehicleData.vehicleId);
+    console.log(this.vehicleData.instructorId);
+    this.InstructorDetails(this.vehicleId) 
   }
+
+
+  
+
+  InstructorDetails(vehicleId) {
+
+    this.vehicleService.getInstructorbyVehicleID(vehicleId).subscribe(
+      response => {
+        console.log("In vInstructorDetails success");
+        console.log(this.vehicleId);
+        this.instructorData = response;
+        console.log(this.instructorData);
+      },
+      error => {
+        console.log(error);
+        console.log("In details error");
+        this.handleErrorResponse(error);
+      }
+    )
+  }
+
+  
+
+
+  //error handling
+  private handleErrorResponse(error: HttpErrorResponse) {
+    this.errorMessage = this.httpError.ErrorResponse(error);
+    console.log(this.errorMessage);
+  };
+
+  closeError() {
+    this.errorMessage = null;
+  }
+
+
 
 }
